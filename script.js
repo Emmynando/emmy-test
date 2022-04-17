98ce08497a406b432003d618ebf05937ac58a093
Emmynando <96875543+Emmynando@users.noreply.github.com>
2022-04-17 01:04:53 +0100

    Website Portfoloio

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mssg = document.querySelector('.msg');
+const contact = document.querySelectorAll('.contact');
+
+
+
+const hue = (min, max) => Math.floor(Math.random() * (max - min) +1) + min;
+const colour = `rgb(${hue(0, 255)}, ${hue(0, 255)}, ${hue(0, 255)})`;
+
+// Contact me DOM
+mssg.addEventListener('click', function(e){
+        mssg.style.backgroundColor = colour;
+        const rect = contact.getBoundingClientRect();
+        e.target.getBoundingClientRect();
+        contact.scrollIntoView({behaviour: 'smooth'});
+});
